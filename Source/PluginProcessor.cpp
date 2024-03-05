2d29e608cb1b90ffd7b111c01182700ba784c5fc
Bartoloměj Pecháček <240191@vutbr.cz>
2024-03-05 19:44:00 +0100

    added custom rotary encoders

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -190,8 +190,8 @@ bool BasicEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* BasicEQAudioProcessor::createEditor()
 {
-    //return new BasicEQAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new BasicEQAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
