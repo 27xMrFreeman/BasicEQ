fc4f46799bd0ee54e5d74a13a3843f0c659643fc
Bartoloměj Pecháček <93556298+27xMrFreeman@users.noreply.github.com>
2025-05-06 22:37:56 +0200

    Added clipping indication

--- Source/HorizontalMeter.cpp
+++ Source/HorizontalMeter.cpp
@@ -22,12 +22,20 @@ void HorizontalMeterLeft::paint(juce::Graphics& g)
     //g.setColour(Colours::white.withBrightness(0.1f));
     //g.fillRoundedRectangle(bounds, 5.f);
 
-    g.setColour(Colour::fromString("FFFCA311"));
     /*g.fillRoundedRectangle(bounds.reduced(0, 50), 17);*/
 
     //g.setColour(Colours::lightgreen);
     const auto scaledX = jmap(level, -60.f, +6.f, 0.f, static_cast<float>(getHeight()));
+    if (level >= 0.f) { g.setColour(juce::Colours::red); }
+    else { g.setColour(Colour::fromString("FFFCA311")); }
     g.fillRoundedRectangle(bounds.removeFromBottom(scaledX), 17.f);
+
+    const auto zeroPos = jmap(0.f, -60.f, +6.f, 0.f, static_cast<float>(getHeight()));
+    r.removeFromBottom(zeroPos);
+    r.removeFromTop(r.getHeight() * 0.95);
+    r.reduce(r.getWidth() * 0.0625, 0);
+    g.setColour(juce::Colour::fromString("FFE5E5E5"));
+    g.fillRect(r);
 
     //g.setColour(Colours::silver);
     //g.drawRoundedRectangle(r, 10.f, 5.f);
@@ -47,11 +55,18 @@ void HorizontalMeterRight::paint(juce::Graphics& g)
     //g.fillRoundedRectangle(bounds, 5.f);
 
     //g.setColour(Colours::lightgreen);
-    g.setColour(Colour::fromString("FFFCA311"));
+    if (level >= 0.f) { g.setColour(juce::Colours::red); }
+    else { g.setColour(Colour::fromString("FFFCA311")); }
+    //g.setColour(Colour::fromString("FFFCA311"));
     const auto scaledX = jmap(level, -60.f, +6.f, 0.f, static_cast<float>(getHeight()));
     g.fillRoundedRectangle(bounds.removeFromBottom(scaledX), 17.f);
 
-
+    const auto zeroPos = jmap(0.f, -60.f, +6.f, 0.f, static_cast<float>(getHeight()));
+    r.removeFromBottom(zeroPos);
+    r.removeFromTop(r.getHeight() * 0.95);
+    r.reduce(r.getWidth() * 0.0625, 0);
+    g.setColour(juce::Colour::fromString("FFE5E5E5"));
+    g.fillRect(r);
     //g.setColour(Colours::silver);
     //g.drawRoundedRectangle(r, 5.f, 5.f);
 
